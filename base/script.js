e28182cd66fe0206e8ef8e1dcfcb3e69fb9151c2
Overlordka <kopary787@gmail.com>
2025-08-18 14:15:13 +0200

    finished

--- base/script.js
+++ base/script.js
@@ -53,7 +53,45 @@ function hendeleMain(data) {
 		<h3>${data.main.benefits.title}</h3>
 		<p>${data.main.benefits.paragraphs}</p>
 	</div>
+
+    <div class="requirements" id="zen-requirements" role="article">
+		<h3>${data.main.requirements.title}</h3>
+		${data.main.requirements.paragraphs.map(paragraph => `<p>${paragraph}</p>`).join("")}
+	</div>
+    <footer>
+        ${data.main.footer.links.map(link => `<a href="${link.href}" title="${link.title}" class="${link.class}">${link.text}</a>`).join(" | ")}	
+    </footer>
+
     `
 }
 
-function handleSidebar(data) {}
+function handleSidebar(data) {
+    sidebar.innerHTML = /*html*/
+    `
+    <div class="wrapper">
+        <div class="design-selection" id="design-selection">
+            <h3>${data.sidebar.designSelection.title}</h3>
+            <nav role="navigation">
+                <ul>
+                    ${data.sidebar.designSelection.items.map(item => `<li>${item}</li>`).join("")}
+                </ul>
+            </nav>
+        </div>
+        <div class="design-archives" id="design-archives">
+            <h3>${data.sidebar.designArchives.title}</h3>
+            <nav role="navigation">
+                <ul>
+                    ${data.sidebar.designArchives.items.map(item => item).join("")}
+                </ul>
+            </nav>
+        </div>
+        <div class="zen-resources" id="zen-resources">
+            <h3>${data.sidebar.zenResources.title}</h3>
+            <nav role="navigation">
+                <ul>
+                    ${data.sidebar.zenResources.items.map(item => item).join("")}
+                </ul>
+            </nav>
+    </div>
+    `
+}
